1f2e1d5d2260ab882d6de278343da5ea8a948601
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-08 18:03:19 +0200

    Update HomePage.jsx

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,40 @@
-import TodoForm from "../../components/TodoForm/TodoForm";
+import { useState, useEffect } from "react";
+
+import TodoForm from "../../components/TodoForm/TodoForm.jsx";
+import TodoList from "../../components/TodoList/TodoList.jsx";
+import s from "./HomePage.module.css";
+import * as api from "../../services/todos-api.js";
 
 const HomePage = () => {
-    return (
-        <>
-            <TodoForm />
-        </>
-    )
-}
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    api
+      .getTodos()
+      .then((response) => setTodos(response))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+    
+    const addNewTodo = (taskText) => {
+        const newTodo = { text: taskText, status: false }; 
+        api
+          .createTodo(newTodo)
+          .then((createdTodo) => {
+            setTodos((prevTodos) => [...prevTodos, createdTodo]); 
+          })
+          .catch((error) => {
+            console.error("Error creating todo:", error);
+          });
+    }
+
+  return (
+    <>
+      <TodoForm onSubmit={addNewTodo}/>
+      <TodoList todos={todos}/>
+    </>
+  );
+};
 
 export default HomePage;
-
