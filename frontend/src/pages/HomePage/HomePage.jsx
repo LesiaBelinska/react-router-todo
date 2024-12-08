ccf956ab89bb0625fcee5e0c3f630ca9f98a7495
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-08 15:25:35 +0200

    Create HomePage.jsx

---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import TodoForm from "../../components/TodoForm/TodoForm";
+
+const HomePage = () => {
+    return (
+        <>
+            <TodoForm />
+        </>
+    )
+}
+
+export default HomePage;
+
