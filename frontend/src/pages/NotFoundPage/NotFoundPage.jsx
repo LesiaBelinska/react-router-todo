5ff67bf3060b36689b62a474ddd77cb2f50c3a95
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-09 10:37:34 +0200

    Create NotFoundPage.jsx

---
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import s from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+    return (
+      <div className={s.container}>
+        <h1 className={s.title}>404 PAGE NOT FOUND</h1>
+      </div>
+    );
+};
+
+export default NotFoundPage;
