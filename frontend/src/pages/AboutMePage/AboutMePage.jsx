fa1073507b13ffaea1a45144de4ad69b138276d4
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-09 10:37:24 +0200

    Create AboutMePage.jsx

---
+++ frontend/src/pages/AboutMePage/AboutMePage.jsx
@@ -0,0 +1,11 @@
+import s from "./AboutMePage.module.css";
+
+const AboutMePage = () => {
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>About Me Page</h1>
+    </div>
+  );
+};
+
+export default AboutMePage;
