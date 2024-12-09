c639446909b5bda8266130b07d3161e482806daa
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-09 10:37:22 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import ContactsPage from "./pages/ContactsPage/ContactsPage.jsx";
+import AboutMePage from "./pages/AboutMePage/AboutMePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <ContactsPage />
+      <AboutMePage />
+      <NotFoundPage />
     </div>
   );
 }
