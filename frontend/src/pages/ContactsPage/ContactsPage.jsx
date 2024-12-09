9135874269836eb9af1afcb3e7ca52070411c481
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-09 10:37:28 +0200

    Create ContactsPage.jsx

---
+++ frontend/src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,12 @@
+import s from "./ContactsPage.module.css";
+
+const ContactsPage = () => {
+
+    return (
+        <div className={s.container}>
+            <h1 className={s.title}>Contacts Page</h1>
+        </div>
+    )
+}
+
+export default ContactsPage;
