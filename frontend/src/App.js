585d4755aaa58ff8ea7d941f5ed1a27ee6e5e687
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-10 09:59:37 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,31 @@
 import { lazy, Suspense } from "react";
-import { createContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import Loader from "./components/Loader/Loader.jsx";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const HomePage = lazy(() =>
+  import("./pages/HomePage/HomePage.jsx")
+);
 
 const ContactsPage = lazy(() =>
   import("./pages/ContactsPage/ContactsPage.jsx")
 );
 
-const AboutMePage = lazy(() => import("./pages/AboutMePage/AboutMePage.jsx"));
+const AboutMePage = lazy(() =>
+  import("./pages/AboutMePage/AboutMePage.jsx")
+);
 
 const NotFoundPage = lazy(() =>
   import("./pages/NotFoundPage/NotFoundPage.jsx")
 );
 
-const ThemeContext = createContext(null);
-
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeProvider>
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -40,7 +37,7 @@ function App() {
         </Routes>
       </Suspense>
       <Toaster />
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
 
