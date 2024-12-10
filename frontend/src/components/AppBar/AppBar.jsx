60892ac9ab767f7ea7c70f2162d5ee0b3c07e187
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-10 09:59:40 +0200

    Update AppBar.jsx

--- frontend/src/components/AppBar/AppBar.jsx
+++ frontend/src/components/AppBar/AppBar.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+
+import { ThemeContext } from "../../context/ThemeContext";
 import Navigation from "../Navigation/Navigation.jsx";
+import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
 import s from "./AppBar.module.css";
 
 const AppBar = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <header className={s.header}>
+    <header className={`${s.header} ${theme === "dark" ? s.dark : s.light}`}>
       <Navigation />
+      <ThemeSwitcher />
     </header>
   );
 };
